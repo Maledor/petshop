eacb77c7e815e792895fc11a9ce7a66b9f29f20f
Maledor <josh.r.cameron@gmail.com>
2017-03-09 19:37:26 -0500

    Wrote a schema for looking up (find) a single id of a document from our database.

--- index.js
+++ index.js
@@ -45,8 +45,20 @@ server.get('/animals', function(req, res){
     }
   });
 });
-//Get /animals/:id
-server.get('/animals/:id', function(req, res){});
+//GET /animals/:id
+server.get('/animals/:id', function(req, res){
+  Animal.find({_id: req.params.id}, function(err, documents){
+    if(err){
+      res.status(500).json({
+        msg: err
+      });
+    } else {
+      res.status(200).json({
+        animals: documents
+      });
+    }
+  });
+});
 //POST /animals
 //PUT /animals/:id
 //DELETE /animals/:id
